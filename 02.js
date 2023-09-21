506e19fea5edd40e6f05ca048b8f65d96be129fc
Ana Julia Menegasso <130059630+AnaJuliaMC@users.noreply.github.com>
2023-09-21 09:09:20 -0300

    ex 02, e 03
    copiei do q eu ja tinha feito

---
+++ 02.js
@@ -0,0 +1,12 @@
+function calculaMedia(a1, a2, a3){
+    let m = (a1+a2+a3) / 3
+    return m
+}
+
+let n1 = 8
+let n2 = 7
+let n3 = 9
+
+let media = calculaMedia(n1, n2, n3)
+
+console.log("a media é:" + media)
---
+++ 03.js
@@ -0,0 +1,8 @@
+let salario = 1000
+console.log("Salario original:"+ salario)
+
+salario = salario * 1.15
+console.log("Salario com aumento:" + salario)
+
+salario = salario - (salario * 0.08)
+console.log("salariom com desconto:" + salario)
